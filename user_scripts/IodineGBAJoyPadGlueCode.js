83b3a73cb09fb0d6fe9d1613e2a0444d123a00c9
Lotionmanss <103595527+Lotionmanss@users.noreply.github.com>
2022-11-02 08:14:31 -0400

    Update IodineGBAJoyPadGlueCode.js

--- user_scripts/IodineGBAJoyPadGlueCode.js
+++ user_scripts/IodineGBAJoyPadGlueCode.js
@@ -27,9 +27,9 @@ var keyZones = [
                 //Down:
                 [40],
                 //R:
-                [83],
+                [36],
                 //L:
-                [65]
+                [35]
 ];
 function keyDown(e) {
     var keyCode = e.keyCode | 0;
